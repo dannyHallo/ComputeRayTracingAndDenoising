7b435b28cf4366d11745a811ad8723b0481c0198
Ruitian Yang <ruitian0716@gmail.com>
2023-07-23 14:52:49 +0800

    cleared unnecesary files

--- src/render-context/RenderSystem.cpp
+++ src/render-context/RenderSystem.cpp
@@ -6,36 +6,6 @@
 #include "RenderSystem.h"
 
 namespace RenderSystem {
-void allocateCommandBuffers(std::vector<VkCommandBuffer> &commandBuffers, uint32_t numBuffers) {
-  commandBuffers.resize(numBuffers);
-  VkCommandBufferAllocateInfo allocInfo{};
-  allocInfo.sType              = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-  allocInfo.commandPool        = vulkanApplicationContext.getCommandPool();
-  allocInfo.level              = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-  allocInfo.commandBufferCount = numBuffers;
-
-  if (vkAllocateCommandBuffers(vulkanApplicationContext.getDevice(), &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
-    throw std::runtime_error("failed to allocate command buffers!");
-  }
-}
-
-void beginCommandBuffer(const VkCommandBuffer &commandBuffer) {
-  VkCommandBufferBeginInfo beginInfo{};
-  beginInfo.sType            = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-  beginInfo.flags            = 0;       // Optional
-  beginInfo.pInheritanceInfo = nullptr; // Optional
-
-  if (vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS) {
-    throw std::runtime_error("failed to begin recording command buffer!");
-  }
-}
-
-void endCommandBuffer(const VkCommandBuffer &commandBuffer) {
-  if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
-    throw std::runtime_error("failed to record command buffer!");
-  }
-}
-
 VkCommandBuffer beginSingleTimeCommands() {
   VkCommandBufferAllocateInfo allocInfo{};
   allocInfo.sType              = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
@@ -67,42 +37,5 @@ void endSingleTimeCommands(VkCommandBuffer commandBuffer) {
   vkQueueWaitIdle(vulkanApplicationContext.getGraphicsQueue());
 
   vkFreeCommandBuffers(vulkanApplicationContext.getDevice(), vulkanApplicationContext.getCommandPool(), 1, &commandBuffer);
-}
-
-void submit(const VkCommandBuffer *commandBuffer, const size_t &numWaitSemaphores, const VkSemaphore *waitSemaphores,
-            const VkPipelineStageFlags *waitStages, const VkSemaphore *signalSemaphores, VkFence &fence) {
-  VkSubmitInfo submitInfo{};
-  submitInfo.sType              = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-  submitInfo.waitSemaphoreCount = static_cast<uint32_t>(numWaitSemaphores);
-  submitInfo.pWaitSemaphores    = waitSemaphores;
-  submitInfo.pWaitDstStageMask  = waitStages;
-
-  submitInfo.commandBufferCount   = 1;
-  submitInfo.pCommandBuffers      = commandBuffer;
-  submitInfo.signalSemaphoreCount = 1;
-  submitInfo.pSignalSemaphores    = signalSemaphores;
-
-  if (vkQueueSubmit(vulkanApplicationContext.getGraphicsQueue(), 1, &submitInfo, fence) != VK_SUCCESS) {
-    throw std::runtime_error("failed to submit draw command buffer!");
-  }
-}
-
-void present(const uint32_t &imageIndex, const VkSemaphore *semaphores, const size_t &numSemaphores) {
-  VkPresentInfoKHR presentInfo{};
-  presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
-
-  presentInfo.waitSemaphoreCount = static_cast<uint32_t>(numSemaphores);
-  presentInfo.pWaitSemaphores    = semaphores;
-  VkSwapchainKHR swapChains[]    = {vulkanApplicationContext.getSwapchain()};
-  presentInfo.swapchainCount     = 1;
-  presentInfo.pSwapchains        = swapChains;
-  presentInfo.pImageIndices      = &imageIndex;
-  presentInfo.pResults           = nullptr;
-
-  VkResult result = vkQueuePresentKHR(vulkanApplicationContext.getPresentQueue(), &presentInfo);
-
-  if (result != VK_SUCCESS) {
-    throw std::runtime_error("failed to present swap chain image!");
-  }
 }
 } // namespace RenderSystem
--- src/render-context/RenderSystem.h
+++ src/render-context/RenderSystem.h
@@ -4,21 +4,10 @@
 #include <vector>
 
 #include "app-context/VulkanApplicationContext.h"
-// #include "scene/Scene.h"
 
+// TODO: enrich this helper class
 namespace RenderSystem {
-void allocateCommandBuffers(std::vector<VkCommandBuffer> &commandBuffers, uint32_t numBuffers);
-
-void beginCommandBuffer(const VkCommandBuffer &commandBuffer);
-
-void endCommandBuffer(const VkCommandBuffer &commandBuffer);
-
 VkCommandBuffer beginSingleTimeCommands();
 
 void endSingleTimeCommands(VkCommandBuffer commandBuffer);
-
-void submit(const VkCommandBuffer *commandBuffer, const size_t &numWaitSemaphores, const VkSemaphore *waitSemaphores,
-            const VkPipelineStageFlags *waitStages, const VkSemaphore *signalSemaphores, VkFence &fence);
-
-void present(const uint32_t &imageIndex, const VkSemaphore *semaphores, const size_t &numSemaphores);
 } // namespace RenderSystem
