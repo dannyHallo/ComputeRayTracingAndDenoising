acb1805e0d34542df4b4349fe675f7c384ea96f6
dannyHallo <ruitian0716@gmail.com>
2023-12-11 22:18:58 +0800

    xmake, make imgui a static lib

--- xmake.lua
+++ xmake.lua
@@ -3,11 +3,20 @@ set_project("vulkan test")
 set_arch("x64")
 set_warnings("all")
 set_languages("c++20")
--- set_toolchains("msvc")
 set_toolchains("clang")
 
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
+
+target("imgui")
+    set_kind("static")
+    add_files(
+        "dep/imgui/imgui_demo.cpp",
+        "dep/imgui/imgui_draw.cpp",
+        "dep/imgui/imgui_tables.cpp",
+        "dep/imgui/imgui_widgets.cpp",
+        "dep/imgui/imgui.cpp"
+    )
 
 target("main")
     -- disable vsprintf warnings in glm lib
@@ -23,12 +32,7 @@ target("main")
     add_files("src/**.cpp")
     add_files(
         "dep/imgui/backends/imgui_impl_glfw.cpp", 
-        "dep/imgui/backends/imgui_impl_vulkan.cpp",
-        "dep/imgui/imgui_demo.cpp",
-        "dep/imgui/imgui_draw.cpp",
-        "dep/imgui/imgui_tables.cpp",
-        "dep/imgui/imgui_widgets.cpp",
-        "dep/imgui/imgui.cpp"
+        "dep/imgui/backends/imgui_impl_vulkan.cpp"
     )
     add_includedirs(
         "src",
@@ -38,10 +42,8 @@ target("main")
         "dep/obj-loader",
         "dep/memory-allocator-hpp"
     )
-    
-    -- add_linkdirs("C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64")
-    -- add_linkdirs("C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.36.32532/lib/x64") -- for msvcrt
-    
+    add_deps("imgui")
+
     add_links( 
     "dep/glfw/Release/glfw3.lib", 
     "User32", 
