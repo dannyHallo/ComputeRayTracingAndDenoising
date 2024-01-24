0fe1b755048e83950fdb737de78e0c2b42b604aa
dannyHallo <ruitian0716@gmail.com>
2024-01-24 13:22:20 +0800

    sort

--- shaders/source/include/definitions.glsl
+++ shaders/source/include/definitions.glsl
@@ -1,5 +1,5 @@
 const vec3 kRgbToLuminanceMat = vec3(0.2989, 0.5870, 0.1140);
-const float kEpsilon          = 1e-8;
+const float kEpsilon          = 3.552713678800501e-15;
 const float kDeg2Rad          = 0.0174532925;
 const float kPi               = 3.1415926535897932384626433832795;
 const float kInvPi            = 0.31830988618379067153776752674503;
