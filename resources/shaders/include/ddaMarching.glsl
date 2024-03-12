1925484133976dc059d3735607525cfb4915b77a
Ruitian Yang <ruitian0716@gmail.com>
2024-03-13 00:06:12 +0800

    a new dda opti method for testing

--- resources/shaders/include/ddaMarching.glsl
+++ resources/shaders/include/ddaMarching.glsl
@@ -41,3 +41,35 @@ bool ddaMarching(out ivec3 oHitChunkPosOffset, out uvec3 oHitChunkLookupOffset, 
   }
   return false;
 }
+
+// branchless DDA
+// originally from https://www.shadertoy.com/view/4dX3zl
+// my fork: https://www.shadertoy.com/view/l3fXWH
+bool ddaMarchingWithSave(out ivec3 oHitChunkPosOffset, out uvec3 oHitChunkLookupOffset,
+                         inout ivec3 mapPos, inout vec3 sideDist, inout bool enteredBigBoundingBox,
+                         vec3 deltaDist, ivec3 rayStep, vec3 o, vec3 d) {
+  bvec3 mask;
+  for (int i = 0; i < MAX_DDA_ITERATION; i++) {
+    ivec3 lookupPos = mapPos + ivec3((sceneInfoBuffer.data.chunksDim - 1) / 2);
+
+    mask = lessThanEqual(sideDist.xyz, min(sideDist.yzx, sideDist.zxy));
+    sideDist += vec3(mask) * deltaDist;
+
+    ivec3 thisMapPos = mapPos;
+    mapPos += ivec3(vec3(mask)) * rayStep;
+
+    if (inChunkRange(lookupPos)) {
+      enteredBigBoundingBox = true;
+      if (hasChunk(lookupPos)) {
+        oHitChunkPosOffset    = thisMapPos;
+        oHitChunkLookupOffset = lookupPos;
+        return true;
+      }
+    }
+    // used to be inside the outer chunk range, but now outside, no need to check further
+    else if (enteredBigBoundingBox) {
+      return false;
+    }
+  }
+  return false;
+}
