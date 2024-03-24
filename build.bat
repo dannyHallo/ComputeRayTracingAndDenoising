8a463c026022ffd7aa3a69fdd79993420550c340
Ruitian Yang <ruitian0716@gmail.com>
2024-03-25 00:51:01 +0800

    build.bat improvement

--- build.bat
+++ build.bat
@@ -1,89 +1,52 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-if "%1"=="debug" (
-    set BUILD_TYPE=debug
-) else if "%1"=="release" (
-    set BUILD_TYPE=release
-) else (
-    echo invalid build type "%1". Exiting...
-    goto :eof
-)
-
 set PROJECT_NAME=voxel-lab
 set PROJECT_GENERATOR=Ninja
+set PROJECT_EXECUTABLE_PATH=build/apps/
+set PROJECT_TARGET_TRIPLET=x64-windows
+set PROJECT_CLANG_PATH=D:/LLVM17/bin/clang.exe
 
-@REM ---------------------------------------------------------------------------------------
+set BUILD_TYPE=release
+set SKIP_CONFIG=OFF
 
-@REM set GLSLC=%VULKAN_SDK%/Bin/glslc.exe
-@REM set SHADERS=chunkFieldConstruction chunkVoxelCreation chunkModifyArg octreeInitNode octreeTagNode octreeAllocNode octreeModifyArg svoCoarseBeam svoTracing temporalFilter aTrous postProcessing 
+FOR %%a IN (%*) DO (
+    if [%%a] == [--debug] set BUILD_TYPE=debug
+    if [%%a] == [--skip-config] set SKIP_CONFIG=ON
+)
 
-@REM https://chromium.googlesource.com/external/github.com/google/shaderc/+/HEAD/glslc/README.asciidoc
-@REM echo compiling shaders...
-@REM if exist "resources/compiled-shaders" rd /s /q "resources/compiled-shaders"
-@REM mkdir "resources/compiled-shaders"
-@REM for %%s in (%SHADERS%) do (
-@REM    echo compiling src/shaders/%%s.comp to resources/compiled-shaders/%%s.spv
-@REM     "%GLSLC%" src/shaders/%%s.comp -o resources/compiled-shaders/%%s.spv --target-env=vulkan1.1
-@REM     if !errorlevel! neq 0 (
-@REM         echo Build failed with error %errorlevel%. Exiting... 
-@REM         goto :eof
-@REM     )
-@REM )
-
-if not exist build mkdir build
-
-@REM ---------------------------------------------------------------------------------------
-
-
-echo:
-echo configuring for the generator %PROJECT_GENERATOR%...
+if %SKIP_CONFIG%==OFF (
 cmake -S . -B build/ ^
     -G %PROJECT_GENERATOR% ^
     -D CMAKE_PROJECT_NAME=%PROJECT_NAME% ^
     -D CMAKE_TOOLCHAIN_FILE="../dep/vcpkg/scripts/buildsystems/vcpkg.cmake" ^
-    -D VCPKG_TARGET_TRIPLET="x64-windows" ^
-    -D CMAKE_CXX_COMPILER="D:/LLVM17/bin/clang.exe" ^
+    -D VCPKG_TARGET_TRIPLET=%PROJECT_TARGET_TRIPLET% ^
+    -D CMAKE_CXX_COMPILER=%PROJECT_CLANG_PATH% ^
     -D CMAKE_EXPORT_COMPILE_COMMANDS=1 ^
     -D CMAKE_CXX_COMPILER_LAUNCHER=ccache ^
     -D CMAKE_BUILD_TYPE=%BUILD_TYPE%
 
 if !errorlevel! neq 0 (
-   echo "CMake config failed with error !errorlevel!. Exiting... "
+   echo cmake config failed with error !errorlevel! aborting...
    goto :eof
 )
+)
 
-echo:
-echo generating project using %PROJECT_GENERATOR%...
-echo ---------------------------------------------------------------------------------------
-echo:
-
+if not exist build mkdir build
 cmake --build build/
 
-set PROJECT_EXECUTABLE_PATH="build/apps/"
-
-@REM ---------------------------------------------------------------------------------------
-
-@REM if %BUILD_TYPE%==release (
-@REM     echo:
-@REM     echo copying resources for release mode...
-@REM     if exist "build/windows/x64/%BUILD_TYPE%/resources" rd /s /q "build/windows/x64/%BUILD_TYPE%/resources"
-@REM     mkdir "build/windows/x64/%BUILD_TYPE%/resources"
-@REM     robocopy "resources" "build/windows/x64/%BUILD_TYPE%/resources" /E /IS /NFL /NDL /NJH /NJS /nc /ns /np
-@REM ) else (
-@REM     echo:
-@REM     echo debug mode, skip resources copying for shader hot-reloading...
-@REM )
-
-@REM @REM ---------------------------------------------------------------------------------------
-
-echo:
-echo run app ...
-echo ---------------------------------------------------------------------------------------
-echo:
+if %BUILD_TYPE%==release (
+    echo:
+    echo copy resources folder for release mode...
+    set RESOURCES_PATH=%PROJECT_EXECUTABLE_PATH%resources
+    echo "!RESOURCES_PATH!"
+    if exist "!RESOURCES_PATH!" rd /s /q "!RESOURCES_PATH!"
+    mkdir "!RESOURCES_PATH!"
+    robocopy "resources/" "!RESOURCES_PATH!" /E /IS /NFL /NDL /NJH /NJS /nc /ns /np
+)
 
 @REM run the application
 @REM /wait blocks the terminal to wait for the application to exit
 @REM /b means to stay in the command line below, 
 @REM /d xxx specifies the startup directory
-start /wait /b /d %PROJECT_EXECUTABLE_PATH% %PROJECT_NAME%.exe
+start /wait /b /d "%PROJECT_EXECUTABLE_PATH%" %PROJECT_NAME%.exe
