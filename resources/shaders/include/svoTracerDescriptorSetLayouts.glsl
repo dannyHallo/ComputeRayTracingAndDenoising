50997334a1441d7cc3c43891aa61f3106abfb4a4
danny <ruitian0716@gmail.com>
2024-07-22 23:34:14 +0800

    opti: auto deduction of the image layouts in glsl side, removed packing function

--- resources/shaders/include/svoTracerDescriptorSetLayouts.glsl
+++ resources/shaders/include/svoTracerDescriptorSetLayouts.glsl
@@ -1,6 +1,8 @@
 #ifndef SVO_TRACER_DESCRIPTOR_SET_LAYOUTS_GLSL
 #define SVO_TRACER_DESCRIPTOR_SET_LAYOUTS_GLSL
 
+#extension GL_EXT_shader_image_load_formatted : require
+
 #include "../include/svoTracerDataStructs.glsl"
 
 layout(binding = 0) uniform RenderInfoUniformBuffer { G_RenderInfo data; }
@@ -14,46 +16,46 @@ temporalFilterInfoUbo;
 layout(binding = 4) uniform SpatialFilterInfoUniformBuffer { G_SpatialFilterInfo data; }
 spatialFilterInfoUbo;
 
-layout(binding = 5, rgba8) readonly uniform image2DArray vec2BlueNoise;
-layout(binding = 42, rgba8) readonly uniform image2DArray vec3BlueNoise;
-layout(binding = 6, rgba8) readonly uniform image2DArray weightedCosineBlueNoise;
+layout(binding = 5) readonly uniform image2DArray vec2BlueNoise;
+layout(binding = 42) readonly uniform image2DArray vec3BlueNoise;
+layout(binding = 6) readonly uniform image2DArray weightedCosineBlueNoise;
 
-layout(binding = 7, r32ui) uniform uimage3D chunksImage;
+layout(binding = 7) uniform uimage3D chunksImage;
 
-layout(binding = 8, r32ui) uniform uimage2D backgroundImage;
-layout(binding = 9, r32f) uniform image2D beamDepthImage;
-layout(binding = 10, r32ui) uniform uimage2D rawImage;
-layout(binding = 11, r32f) uniform image2D depthImage;
-layout(binding = 12, r32ui) uniform uimage2D octreeVisualizationImage;
-layout(binding = 13, r8ui) uniform uimage2D hitImage;
-layout(binding = 14, r8ui) uniform uimage2D temporalHistLengthImage;
-layout(binding = 15, rgba16f) uniform image2D motionImage;
-layout(binding = 16, r32ui) uniform uimage2D normalImage;
-layout(binding = 17, r32ui) readonly uniform uimage2D lastNormalImage;
-layout(binding = 18, rgba32f) uniform image2D positionImage;
-layout(binding = 19, rgba32f) uniform image2D lastPositionImage;
-layout(binding = 20, r32ui) uniform uimage2D voxHashImage;
-layout(binding = 21, r32ui) readonly uniform uimage2D lastVoxHashImage;
-layout(binding = 22, r32ui) uniform uimage2D accumedImage;
-layout(binding = 23, r32ui) readonly uniform uimage2D lastAccumedImage;
+layout(binding = 8) uniform image2D backgroundImage;
+layout(binding = 9) uniform image2D beamDepthImage;
+layout(binding = 10) uniform image2D rawImage;
+layout(binding = 11) uniform image2D depthImage;
+layout(binding = 12) uniform image2D octreeVisualizationImage;
+layout(binding = 13) uniform uimage2D hitImage;
+layout(binding = 14) uniform uimage2D temporalHistLengthImage;
+layout(binding = 15) uniform image2D motionImage;
+layout(binding = 16) uniform uimage2D normalImage;
+layout(binding = 17) readonly uniform uimage2D lastNormalImage;
+layout(binding = 18) uniform image2D positionImage;
+layout(binding = 19) uniform image2D lastPositionImage;
+layout(binding = 20) uniform uimage2D voxHashImage;
+layout(binding = 21) readonly uniform uimage2D lastVoxHashImage;
+layout(binding = 22) uniform image2D accumedImage;
+layout(binding = 23) readonly uniform image2D lastAccumedImage;
 
 // these are not uint encoded because they need to be SAMPLED as textures, see lastTaaTexture
-layout(binding = 24, rgba16f) uniform image2D taaImage;
-layout(binding = 25, rgba16f) uniform image2D lastTaaImage;
+layout(binding = 24) uniform image2D taaImage;
+layout(binding = 25) uniform image2D lastTaaImage;
 
-layout(binding = 26, r32ui) uniform uimage2D blittedImage;
+layout(binding = 26) uniform image2D blittedImage;
 
-layout(binding = 27, r32ui) uniform uimage2D aTrousPingImage;
-layout(binding = 28, r32ui) uniform uimage2D aTrousPongImage;
-layout(binding = 29, r32ui) uniform uimage2D aTrousFinalResultImage;
+layout(binding = 27) uniform image2D aTrousPingImage;
+layout(binding = 28) uniform image2D aTrousPongImage;
+layout(binding = 29) uniform image2D aTrousFinalResultImage;
 
-layout(binding = 30, rgba8) uniform image2D renderTargetImage;
+layout(binding = 30) uniform image2D renderTargetImage;
 
 layout(binding = 31) uniform sampler2D lastTaaTexture;
 
-layout(binding = 36, rgba32f) uniform image2D transmittanceLutImage;
-layout(binding = 37, rgba32f) uniform image2D multiScatteringLutImage;
-layout(binding = 38, rgba32f) uniform image2D skyViewLutImage;
+layout(binding = 36) uniform image2D transmittanceLutImage;
+layout(binding = 37) uniform image2D multiScatteringLutImage;
+layout(binding = 38) uniform image2D skyViewLutImage;
 // corresponding sampler for the above images
 layout(binding = 39) uniform sampler2D transmittanceLutTexture;
 layout(binding = 40) uniform sampler2D multiScatteringLutTexture;
