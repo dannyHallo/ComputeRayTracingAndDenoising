d5467f58ab58f32f3a3bd0d34bb9bf1d3bd3f4db
danny <ruitian0716@gmail.com>
2024-08-05 02:25:51 +0800

    adjustable packing parameters, smooth surface

--- resources/shaders/include/blockTypes.glsl
+++ resources/shaders/include/blockTypes.glsl
@@ -15,26 +15,31 @@ uint getBlockTypeFromWeight(float weight) {
   return kBlockTypeRock;
 }
 
-const float f = 1.0 / 100.0;
+// these are adjustable
+const float f           = 1.0 / 100.0;
 const float boundaryMin = -f;
 const float boundaryMax = f;
+const uint nBits        = 12;
+const uint nLevels      = (1 << nBits) - 1;
+const uint encodedMask  = nLevels;
 
 uint packWeight(float weight) {
-  weight = clamp(weight, boundaryMin, boundaryMax);
-  return uint((weight - boundaryMin) / (boundaryMax - boundaryMin) * 255.0);
+  weight          = clamp(weight, boundaryMin, boundaryMax);
+  const float f01 = (weight - boundaryMin) / (boundaryMax - boundaryMin);
+  return uint(f01 * nLevels);
 }
 
 float unpackWeight(uint encodedWeight) {
-  return (float(encodedWeight) / 255.0) * (boundaryMax - boundaryMin) + boundaryMin;
+  return (float(encodedWeight) / float(nLevels)) * (boundaryMax - boundaryMin) + boundaryMin;
 }
 
 uint packBlockTypeAndWeight(uint blockType, float weight) {
-  return (blockType << 8) | packWeight(weight);
+  return (blockType << nBits) | packWeight(weight);
 }
 
 void unpackBlockTypeAndWeight(out uint oBlockType, out float oWeight, uint data) {
-  oBlockType = (data >> 8) & 0xFF;
-  oWeight    = unpackWeight(data & 0xFF);
+  oBlockType = (data >> nBits);
+  oWeight    = unpackWeight(data & encodedMask);
 }
 
 #endif // BLOCK_TYPES_GLSL
